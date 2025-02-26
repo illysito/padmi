e4787fc0affdf8da01f91ca5bc93c2b13dd09be9
illysito <illyalukyanovdesign@gmail.com>
2025-02-26 13:30:18 +0000

    Update universe.js

--- src/features/3d/world/universe.js
+++ src/features/3d/world/universe.js
@@ -1,24 +1,30 @@
 import { World } from './World.js' // cambiar la ruta si fuera necesario
 
 function world(container, shader_index) {
+  function isDesktop() {
+    //prettier-ignore
+    return window.innerWidth >= 991
+  }
   // 1. Create an instance of the World app
-  const world = new World(container, shader_index)
-  // 2. Render the scene
-  // world.start()
-  const observer = new IntersectionObserver(
-    (entries) => {
-      entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-          world.start() // Resume rendering when visible
-        } else {
-          world.stop() // Pause rendering when out of view
-        }
-      })
-    },
-    { threshold: 0.4 } // Adjust threshold for sensitivity
-  )
+  if (isDesktop()) {
+    const world = new World(container, shader_index)
+    // 2. Render the scene
+    // world.start()
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            world.start() // Resume rendering when visible
+          } else {
+            world.stop() // Pause rendering when out of view
+          }
+        })
+      },
+      { threshold: 0.4 } // Adjust threshold for sensitivity
+    )
 
-  observer.observe(container)
+    observer.observe(container)
+  }
 }
 
 export default world
