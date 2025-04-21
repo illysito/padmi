39bd269628ed70ae93a476fb4fd977053229f54f
illysito <illyalukyanovdesign@gmail.com>
2025-04-21 23:36:16 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -23,6 +23,10 @@ const world_container = document.querySelector('.world-container')
 const world_2_container = document.querySelector('.world-2-container')
 const particles_container = document.querySelector('.particles-container')
 
+window.onbeforeunload = () => {
+  window.scrollTo(0, 0)
+}
+
 function runGeneralFunctions() {
   world_2(world_2_container)
   nav()
